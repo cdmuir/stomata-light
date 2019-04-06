31dc9be5780064025b8fa242dfd6eeaa70ebc741
cdmuir <chrisdmuir@gmail.com>
2019-04-05 21:39:15 -1000

    minor change

--- r/header.R
+++ r/header.R
@@ -2,13 +2,10 @@ rm(list = setdiff(ls(), "run"))
 graphics.off()
 
 # remove before final publication
-file.copy(
-  "/Volumes/GoogleDrive/My Drive/resources/bibliography/refs.bib",
-  "/Volumes/GoogleDrive/My Drive/research/01_active/stomata-light/ms/refs.bib", 
-  overwrite = TRUE)
-
-file.exists("/Volumes/GoogleDrive/My Drive/professional/resources/bibliography/refs.bib")
-message("delete PAT and make leafoptimizer public before putting on GitHub")
+# file.copy(
+#   "/Volumes/GoogleDrive/My Drive/resources/bibliography/refs.bib",
+#   "/Volumes/GoogleDrive/My Drive/research/01_active/stomata-light/ms/refs.bib", 
+#   overwrite = TRUE)
 
 # Libraries
 library(cowplot)
